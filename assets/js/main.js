fbb93e6a341c0072671b4128c023c574ed8dd557
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2025-02-21 20:59:48 -0600

    Navbar responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,22 @@
 const navLinks = document.querySelectorAll(".nav-link");
+const openMobileMenu = document.querySelector("#open-menu");
+const closeMobileMenu = document.querySelector("#close-menu");
+const mobileMenu = document.querySelector("#mobile-menu");
+const toggleMobileSubmenu = document.querySelector("#toggle-mobile-submenu");
+const mobileSubmenu = document.querySelector("#mobile-submenu");
+const mobileLinks = document.querySelectorAll(".mobile-link");
+
+function hideMobileMenu() {
+    mobileMenu.classList.add("hidden");
+    mobileMenu.classList.remove("block");
+    document.body.style.overflow = 'auto';
+}
+
+function showMobileMenu() {
+    mobileMenu.classList.add("block");
+    mobileMenu.classList.remove("hidden");
+    document.body.style.overflow = 'hidden';
+}
 
 window.addEventListener("scroll", () => {
     const navBar = document.querySelector("#nav-bar");
@@ -18,4 +36,16 @@ navLinks.forEach(navLink => {
 
         navLink.classList.add("active");
     });
+});
+
+openMobileMenu.addEventListener("click", showMobileMenu);
+
+closeMobileMenu.addEventListener("click", hideMobileMenu);
+
+mobileLinks.forEach(link => {
+    link.addEventListener("click", hideMobileMenu)
+})
+
+toggleMobileSubmenu.addEventListener("click", ()=> {
+    mobileSubmenu.classList.toggle("active");
 });
